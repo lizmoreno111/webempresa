37c02d01702cdb77a8c0f441e5d4d93c88e179d4
Lizbeth Moreno <lizbethmoreno989@gmail.com>
2020-05-25 14:50:22 -0500

    Se realiza cambio

--- webempresa/urls.py
+++ webempresa/urls.py
@@ -21,10 +21,20 @@ from django.conf import settings
 
 
 urlpatterns = [
+
+    #core
     path('', include('core.urls')),
+
+    #services
+    path('services/', include('services.urls')),
+
+    #admin
     path('admin/', admin.site.urls),
+
+    
+    
 ]
 
 if settings.DEBUG:
     from django.conf.urls.static import static 
-    urlpatterns += static(settings.MEDIA_URL, documen_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
